a8d75237a121dac32022585dc6c579d66f67a133
Diego Meza <diego.meza.ext@geodis.com>
2021-10-25 07:48:15 -0500

    3d UI 1.0 working

--- src/App.tsx
+++ src/App.tsx
@@ -12,32 +12,35 @@ import './App.css';
 function animate () {
   requestAnimationFrame(animate);
   controls.update();
-  // this.state.brainStormNoteBook.notes.forEach((note, i) => {
-  //     const temporalVector = new THREE.Vector3()
-  //     // get the position of the center of the cube
-  //     note.mesh.updateWorldMatrix(true, false)
-  //     note.mesh.getWorldPosition(temporalVector)
-  //     // get the normalized screen coordinate of that position
-  //     // x and y will be in the -1 to +1 range with x = -1 being
-  //     // on the left and y = -1 being on the bottom
-  //     temporalVector.project(this.camera)
+  // console.log(dotsGroup)
+  dotsGroup.children.forEach((mesh: any) => {
+      const temporalVector = new THREE.Vector3()
+      // get the position of the center of the cube
+      mesh.updateWorldMatrix(true, false)
+      mesh.getWorldPosition(temporalVector)
+      // get the normalized screen coordinate of that position
+      // x and y will be in the -1 to +1 range with x = -1 being
+      // on the left and y = -1 being on the bottom
+      temporalVector.project(camera)
 
-  //     // convert the normalized position to CSS coordinates
-  //     const x = (temporalVector.x * 0.5 + 0.5) * this.renderer.domElement.clientWidth
-  //     const y = (temporalVector.y * -0.7 + 0.5) * this.renderer.domElement.clientHeight
-  //     //console.log(x, y)
-  //     // move the elem to that position
-  //     // console.log(this.$refs['node-label-' + i][0])
-  //     var _label =  document.getElementById(note.uuid); // this._refs.get(note.uuid);
-  //     // console.log(_label)
-  //     if (_label) _label.style.transform = `translate(-50%, -50%) translate(${x}px,${y}px)`;
-  //   });
+      // convert the normalized position to CSS coordinates
+      const x = ((temporalVector.x * 0.5)) * renderer.domElement.clientWidth
+      const y = ((temporalVector.y * -0.7)) * renderer.domElement.clientHeight
+      //console.log(x, y)
+      // move the elem to that position
+      // console.log(this.$refs['node-label-' + i][0])
+      var _label =  document.getElementById(mesh.name); // this._refs.get(note.uuid);
+      // console.log(_label)
+      if (_label) _label.style.transform = `translate(-50%, -50%) translate(${x}px,${y}px)`;
+    });
   renderer.render( scene, camera );
 };
+const dotsGroup = new THREE.Group();
 const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
 const sphereGeometry = new THREE.SphereGeometry( 0.1, 32, 32 );
 const meshMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
 const scene = new THREE.Scene();
+scene.add(dotsGroup)
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.zoom = 1;
 const renderer = new THREE.WebGLRenderer();
@@ -47,32 +50,25 @@ document.body.appendChild( renderer.domElement );
 camera.position.z = 5;
 animate();
 window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }, false);
 
 function App() {
-  // const [notes, setNotes] = useState(new Immutable.Map<string, Notes>())
-  const [title, setTitle] = useState('')
-  const [content, setContent] = useState('')
-  var notesjsx: JSX.Element[] = [];
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  function setNote(title: string) {
+    const note = Notebook.notes.get(title);
+    if (!note) return;
+    setTitle(note.title);
+    setContent(note.content);
+  }
+  var labels: JSX.Element[] = [];
   Notebook.notes.forEach((note, key, map) => {
-    notesjsx.push(
-      <li key={key}>
-        <h3>{note.title}</h3>  
-        <p>{note.content}</p>
-        <p><strong>mentions: {note.mentions().join(',')}</strong></p>
-        {<p><strong>references: {note.references().join(',')}</strong></p>}
-      </li>
-    );
+    labels.push(<label onClick={() => setNote(key)} key={key} id={key}>{key}</label>)
   });
-  var dirjsx: JSX.Element[] = [];
-  Directory.dir.forEach((value, key, map) => {
-    dirjsx.push(
-      <small>{key}, </small>
-    );
-  });
+  var hub = <div className="hub"><p>Notes: {Notebook.notes.size}</p></div>
   function drawLines(note: any, _dot: any = null, ref = false) {
     const dot = _dot || scene.getObjectByName(note.title); 
     const groupName = `${note.title}-mentions`;
@@ -101,7 +97,7 @@ function App() {
     dot.translateY(Math.random() * 2);
     dot.translateZ(Math.random() * 2);
     dot.name = note.title;
-    scene.add(dot);
+    dotsGroup.add(dot);
     return drawLines(note, dot, true);
   }
   function newNote(title: string, content: string, event: any) {
@@ -113,8 +109,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {<ol>{notesjsx}</ol>}
-        {/*<p>{dirjsx}</p>*/}
+        {hub}
+        {labels}
         <form className="note-form" onSubmit={ event => newNote(title, content, event) }>
           <input placeholder="title" value={ title } onChange={ event => setTitle(event.target.value) }/>
           <textarea placeholder="content" value={ content } onChange={ event => setContent(event.target.value) }></textarea>
