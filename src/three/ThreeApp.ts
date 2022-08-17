5c398de64ec53089470a6aedb3b376351f1273e1
DiegoMeza <hi@diego.pro>
2022-08-16 22:54:59 -0500

    remove axis helper

--- src/three/ThreeApp.ts
+++ src/three/ThreeApp.ts
@@ -18,7 +18,7 @@ const meshSelectedMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer() // ({ alpha: true });
 const axesHelper = new THREE.AxesHelper( 0.3 );
-scene.add( axesHelper );
+// scene.add( axesHelper );
 const controls = new OrbitControls( camera, renderer.domElement );
 var selectedMesh: any = null;
 var INITIALIZED: boolean = false;
